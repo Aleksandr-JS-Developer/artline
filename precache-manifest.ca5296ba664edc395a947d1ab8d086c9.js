aec04c69e5663aaf94f5e597dc14d4938546220a
Romantsov Alexander <script@vip-person.net>
2021-11-06 22:29:54 +0200

    nitial dist subtree commit

---
+++ precache-manifest.ca5296ba664edc395a947d1ab8d086c9.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "443545130ca7620b5dc0",
+    "url": "/artline/css/app.38ab7e2e.css"
+  },
+  {
+    "revision": "4d29a6fa1f80cd2f258a",
+    "url": "/artline/css/chunk-06ca9ab9.462b9b75.css"
+  },
+  {
+    "revision": "adf1f121ddfae160139f",
+    "url": "/artline/css/chunk-43ec1c1c.6ed05a36.css"
+  },
+  {
+    "revision": "209eb1e43319cb061d9d",
+    "url": "/artline/css/chunk-486901cb.139cd558.css"
+  },
+  {
+    "revision": "0dcf186dd5d1a2851762",
+    "url": "/artline/css/chunk-d08a455a.04af2a3d.css"
+  },
+  {
+    "revision": "aa18c6afe27431297db0",
+    "url": "/artline/css/chunk-vendors.310a995d.css"
+  },
+  {
+    "revision": "1b61b4283d5eb3e7e9fa71082c21259b",
+    "url": "/artline/index.html"
+  },
+  {
+    "revision": "443545130ca7620b5dc0",
+    "url": "/artline/js/app.0c299bb1.js"
+  },
+  {
+    "revision": "4d29a6fa1f80cd2f258a",
+    "url": "/artline/js/chunk-06ca9ab9.05fe9cfd.js"
+  },
+  {
+    "revision": "adf1f121ddfae160139f",
+    "url": "/artline/js/chunk-43ec1c1c.a493efad.js"
+  },
+  {
+    "revision": "209eb1e43319cb061d9d",
+    "url": "/artline/js/chunk-486901cb.f941d76e.js"
+  },
+  {
+    "revision": "0dcf186dd5d1a2851762",
+    "url": "/artline/js/chunk-d08a455a.dae6b449.js"
+  },
+  {
+    "revision": "aa18c6afe27431297db0",
+    "url": "/artline/js/chunk-vendors.1e3e9efd.js"
+  },
+  {
+    "revision": "c5487a4984299f8b895c67e8ed9cb2b7",
+    "url": "/artline/manifest.json"
+  },
+  {
+    "revision": "8dce4cad2e57d3ac440586765a417ad3",
+    "url": "/artline/robots.txt"
+  }
+]);
